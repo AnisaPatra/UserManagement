2cebd1c42787a048b7055e0b814a5a7831d6dd44
Anisa <anipatra2000@gmail.com>
2024-02-19 20:31:23 +0530

    build: added frontend files

--- src/users-frontend/src/App.js
+++ src/users-frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserListPage from './pages/UserListPage';
+import UserDetailsPage from './pages/UserDetailsPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/users' component={UserListPage} />
+          <Route exact path='/users/:username' component={UserDetailsPage} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
