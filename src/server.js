97ea430863c7aa1613bc89c19d2452d1957adc9b
Anisa <anipatra2000@gmail.com>
2024-02-19 17:08:59 +0530

    build: setup server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()); //Restrict cors origin requests
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
